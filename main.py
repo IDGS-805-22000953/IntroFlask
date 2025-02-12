e079409a33bc9d867f7e8718ec062c2396ec14ed
IDGS-805-2200095 <79194@alumnos.utleon.edu.mx>
2025-02-11 23:39:25 -0600

    Commit del 11 de febrero, cinepolis flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app=Flask(__name__)
 
@@ -51,6 +51,81 @@ def form1():
             </form>
     
            '''
+           
+           
+           
+@app.route("/OperasBas")
+def operas():
+    return render_template("OperasBas.html")
+
+@app.route("/resultado" ,methods=["POST"])
+def result():
+    if request.method == "POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+        operacion = request.form.get("operacion")
+        
+        if operacion == "multiplicacion":
+            return f"La multiplicacion de {num1} x {num2} = {str(int(num1)*int(num2))}"
+        
+        elif operacion == "division":
+            return f"La division de {num1} / {num2} = {str(int(num1)/int(num2))}"
+        
+        elif operacion == "resta":
+            return f"La resta de {num1} - {num2} = {str(int(num1)-int(num2))}"
+        
+        elif operacion == "suma":
+            return f"La suma de {num1} + {num2} = {str(int(num1)+int(num2))}"
+        
+
+
+
+@app.route("/Cine")
+def cine():
+    return render_template("cine.html")
+
+@app.route("/entradas", methods=["GET", "POST"])
+def entrada():
+    if request.method == "POST":
+        try:
+           
+            compradoresC = int(request.form.get("compradoresC", 0))
+            boletos = int(request.form.get("boletos", 0))
+            metodo_pago = request.form.get("tarjeta")  
+            proceso = request.form.get("proceso")
+
+            if proceso == "procesar":
+                precio_unitario = 12
+                total_global = boletos * precio_unitario
+
+                
+                if boletos <= 7 * compradoresC:
+                    if 3 <= boletos <= 5:
+                        total_global -= total_global * 0.10  
+                    elif boletos >= 6:
+                        total_global -= total_global * 0.15  
+
+                
+                if metodo_pago == "si":
+                    total_global -= total_global * 0.10 
+
+              
+                total_global = round(total_global, 2)
+
+                return render_template("cine.html", valorPago=total_global)
+
+        except ValueError:
+            return "Error: Ingrese valores numéricos válidos para compradores y boletos."
+
+    return render_template("cine.html", valorPago=0)
+
+            
+            
+            
+            
+        
+        
+
 
 
 if __name__=="__main__":
